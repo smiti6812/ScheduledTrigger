a183eeeb20f9c2dcd718aea3a99df66a9579aca9
smiti6812 <73239938+smiti6812@users.noreply.github.com>
2024-08-06 10:36:11 +0200

    Update startSonar.ps1

--- .github/workflows/Sonar/bin/startSonar.ps1
+++ .github/workflows/Sonar/bin/startSonar.ps1
@@ -1,3 +1,3 @@
 $actionPath = Join-Path -Path  "$env:actionPath"  -ChildPath "\bin\initializeSonar.ps1"
-$myFoldePath = $actionPath
+$myFoldePath = & "$PSScriptRoot\bin\initializeSonar.ps1"
 Write-Output sonarPath=$myFoldePath >> $env:GITHUB_OUTPUT
